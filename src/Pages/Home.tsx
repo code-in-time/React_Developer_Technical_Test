5757851648af7c4ea8b077d74455f4ae8b0df808
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-10-08 10:37:15 +0100

    reducer concept

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux'
 
-const Home = () => {
+const Home = (props: any) => {
+
+  useEffect(() => {
+
+    // Get the list of breed groups. Call the thunk
+
+
+  },);
+
   return (
     <div>
-      Home page
+      Home page {props.breeds}
     </div>
   );
 };
 
-export default Home;
+const mapStateToProps = (state: any) => ({
+  breeds: state.Breeds.allBreeds
+});
+
+const mapDispatchToProps = {
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import todo from "./todo";
 import todo2 from "./todo2";
+import Breeds from './Breeds'
 
 export default combineReducers({
   todo,
-  todo2
+  todo2,
+  Breeds
 });
