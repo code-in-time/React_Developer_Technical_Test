28f4aaf67eecfbdef617576c55c36d92a4e2f104
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-10-08 15:00:24 +0100

    Show sub breeds

--- src/Pages/BreedImages.tsx
+++ src/Pages/BreedImages.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { useParams } from "react-router-dom";
 
@@ -15,8 +15,31 @@ const BreedImages = (props: any) => {
     props.thunkGetBreedSubImages(breedGroup);
   }, []);
 
+  const generateSubBreeds = (allBreeds: any) => {
+    const subBreeds = allBreeds[breedGroup];
+    if (subBreeds === undefined || subBreeds.length === 0) {
+      return '';
+    }
+    // const data = subBreeds.map((item: any) => (<span className="text-color-brown">{item}</span>))
+
+    const data = subBreeds.map((item: any, index: number) => (
+      <>
+        {/* TODO remove last comma */}
+        <span className="text-color-brown">{item}</span> <span>, </span>
+     
+      </>
+    ))
+
+    return <p>Sub breads: {data}</p>
+  }
+
   return (
     <Template1 className="BreedImages">
+      
+      <h2 className="text-capital">{breedGroup}</h2>
+
+      {generateSubBreeds(props.allBreeds)}
+
       <div>
         Breed Images {breedGroup}
       </div>
@@ -25,6 +48,7 @@ const BreedImages = (props: any) => {
 };
 
 const mapStateToProps = (state: any) => ({
+  allBreeds: state.Breeds.allBreeds,
   subBreedImages: state.Breeds.subBreedImages
 });
 
